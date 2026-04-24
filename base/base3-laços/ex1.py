70967c27d92902d310b51b967e3e61dc9c568fce
João Pedro <joaopedrocva21@gmail.com>
2026-04-23 22:11:11 -0300

    começando a treinar laços

---
+++ base/base3-laços/ex1.py
@@ -0,0 +1,16 @@
+clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+
+for nome in clientes:
+    print(nome)
+
+"""
+FAZENDO COM WHILE:
+
+clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+
+i = 0
+
+while i < len(clientes):
+    print(clientes[i])
+    i += 1
+"""
